866dbed42113564eda6eaf68e8b013040a448dd2
jasmine carpentier <jascarpentier@gmail.com>
2019-07-22 14:57:14 -0400

    footer social icons

--- client/src/components/footer/HireUs.jsx
+++ client/src/components/footer/HireUs.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import './style.css';
 
-export default function HireUs(){
-    return(
-        <div>
-            <h4>Hire us</h4>
-        </div>
+export default function HireUs() {
+  return (
+    <div className='Footer'>
+      <div>
+        <h4>Hire us</h4>
+      </div>
 
-    )
+      <div class="footer-social-links">
+        <a href="https://www.facebook.com/" title="Facebook" target="_blank"><i className="fa fa-facebook"></i></a>
+        <a href="/" title="Twitter" target="_blank"><i className="fa fa-twitter"></i></a>
+        <a href="/" title="GitHub" target="_blank"><i className="fa fa-github"></i></a>
+        <a href="/" title="LinkedIn+" target="_blank"><i className="fa fa-linkedin"></i></a>
+      </div>
+    </div>
+
+  )
 }
