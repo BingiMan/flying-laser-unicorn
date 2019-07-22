2ce8fcdafe03a5c37b6256e7d9ecf87f6d7bc135
bingi <luis.carbonel.gk@hotmail.com>
2019-07-21 20:50:15 -0400

    setting up stylesheet

--- client/src/components/main/Home.jsx
+++ client/src/components/main/Home.jsx
@@ -4,7 +4,7 @@ import Introduction from './Introduction'
 export default function Home(){
     return(
         <section>
-            <div>
+            <div className={home}>
                 <h1>HOME COMPONENT! YAY!</h1>
                 <Introduction/>
             </div>
