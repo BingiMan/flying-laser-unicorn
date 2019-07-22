83d7ab0eba4ab77603e82368bed5a13ad66c5367
bingi <luis.carbonel.gk@hotmail.com>
2019-07-22 13:38:55 -0400

    comments form

---
+++ client/src/components/main/CommentsForm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const CommentsForm = (props) => {
+    return(
+        <form>
+            <h1>COMMENTS HERE...</h1>
+            <input name="message" type="text" placeholder="text here ..." onChange={props.handleChange}/>
+            <input name="yeanay" type="text" placeholder=" " onChange={props.handleChange}/>
+            <input type="submit" placeholder="Submit"/>
+        </form>
+    );
+};
