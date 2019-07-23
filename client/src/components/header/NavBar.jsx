f7aed8d2087461c02c730c5e1da8ac83416f8486
Christopher Montalbo <montyrivers@Christophers-MacBook-Pro-2.local>
2019-07-23 11:46:38 -0400

    bug fixes in app, api-calls, eateries component

--- client/src/components/header/NavBar.jsx
+++ client/src/components/header/NavBar.jsx
@@ -1,49 +1,50 @@
 
-import {Link} from "react-router-dom";
+import React from 'react'
+import { Link } from "react-router-dom";
 
 export default class NavigationBar extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.tabs = [
-            {
-                link: '/',
-                label: 'Home',
-            },
-            {
-                link: '/introduction',
-                label: 'Introduction'
-            },
-            {
-                link: '/addEatery',
-                label: 'Eatery',
-            },
-            {
-                link: '/login',
-                label: 'Loggin',
-            },
-            {
-                link: '/register',
-                label: 'Register',
-            },
-        ]
+    this.tabs = [
+      {
+        link: '/',
+        label: 'Home',
+      },
+      {
+        link: '/introduction',
+        label: 'Introduction'
+      },
+      {
+        link: '/addEatery',
+        label: 'Eatery',
+      },
+      {
+        link: '/login',
+        label: 'Loggin',
+      },
+      {
+        link: '/register',
+        label: 'Register',
+      },
+    ]
+  }
+
+  linkOrButton(tab) {
+    if (tab.link) {
+      return <Link to={tab.link}>{tab.label}</Link>
+    } else {
+      return <button>{tab.label}</button>
     }
+  }
 
-    linkOrButton(tab) {
-        if (tab.link) {
-            return <Link to={tab.link}>{tab.label}</Link>
-        } else {
-            return <button>{tab.label}</button>
+  render() {
+    return (
+      <nav>
+        {
+          this.tabs.map(tab => this.linkOrButton(tab))
         }
-    }
-
-    render() {
-        return (
-            <nav>
-                {
-                    this.tabs.map(tab => this.linkOrButton(tab))
-                }
-            </nav>
-        );
-    }
+      </nav>
+    );
+  }
 }
