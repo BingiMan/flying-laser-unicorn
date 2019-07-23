cd963de2f32b2e037cb73758f360d0da36497977
jasmine carpentier <jascarpentier@gmail.com>
2019-07-23 13:16:58 -0400

    css intro

--- client/src/components/main/Introduction.jsx
+++ client/src/components/main/Introduction.jsx
@@ -11,9 +11,44 @@ export default function Introduction() {
           Out of ideas of where to go and need inspiration? <br />
           Our mission is to help you discover new and delicious places to eat on your lunch break.</h4>
       </div>
-      <div className='intro-image'>
+
+
+      <div className='about-team-intro'>
+
+        <h2>Meet the team</h2>
+
+      </div>
+
+
+
+      <div className='team-intro'>
+
+        <img className='single-team-img' src='https://i.imgur.com/SAypr2F.jpg' alt='team-pandas' />
+
+        <a href="https://github.com/fallwall">
+          <button className='team-button'>Tibby</button>
+        </a>
+
+        <img className='single-team-img' src='https://i.imgur.com/SAypr2F.jpg' alt='team-pandas' />
+
+
+        <a href="https://github.com/BingiMan">
+          <button className='team-button'>Luis</button>
+        </a>
+
+        <img className='single-team-img' src='https://i.imgur.com/SAypr2F.jpg' alt='team-pandas' />
+        <a href="https://github.com/Montyrivers">
+          <button className='team-button'>Chris</button>
+        </a>
+
+        <img className='single-team-img' src='https://i.imgur.com/SAypr2F.jpg' alt='team-pandas' />
+
+        <a href="https://github.com/jascarpentier">
+          <button className='team-button'>Jasmine</button>
+        </a>
 
       </div>
     </div>
+
   )
 }
