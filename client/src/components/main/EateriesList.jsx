1b5cd6857bc329b43c33cd8c9cc48463a57f7281
XX <xx@XXs-MBP13.local>
2019-07-25 13:08:42 -0400

    commented out query selector

--- client/src/components/main/EateriesList.jsx
+++ client/src/components/main/EateriesList.jsx
@@ -6,9 +6,9 @@ export default function EateriesList(props) {
     <>
       {props.eateries.map(eatery =>
         <div className="eatery" key={eatery.id}>
-          <h3> {eatery.name} </h3>
-          <p>{eatery.address}</p>
-          <p>{eatery.priceRange}</p>
+          <h3> <span>Name:</span> {eatery.name} </h3>
+          <p><span>Address:</span> {eatery.address}</p>
+          <p><span>Price Range:</span>{eatery.priceRange}</p>
           <Link 
           to= {`/single-eatery/${eatery.id}`}> Details </Link>
 
