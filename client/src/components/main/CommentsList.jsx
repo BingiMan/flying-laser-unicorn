1f9bfbe020a2d06df08e2ccc55c7ad56545626c4
jasmine carpentier <jascarpentier@gmail.com>
2019-07-25 12:26:00 -0400

    css i think

--- client/src/components/main/CommentsList.jsx
+++ client/src/components/main/CommentsList.jsx
@@ -6,12 +6,12 @@ export default function CommentsList(props) {
       {/* <h1>comment list below</h1> */}
       {props.comments.map(comment =>
         <div className="comment" key={comment.id}>
-          <h3>{comment.yaynay.toString()}</h3>
+          {/* <h3>{comment.yaynay.toString()}</h3> */}
           <h1> {comment.message} </h1>
           {props.editingId === '' &&
-            <button onClick={() => props.handleDelete(comment.id)}> Delete </button>}
+            <button className='deleteComment' onClick={() => props.handleDelete(comment.id)}> Delete </button>}
           {props.editingId === '' &&
-            <button onClick={() => props.handleUpdate(comment.id)}> Update </button>}
+            <button className='updateComment' onClick={() => props.handleUpdate(comment.id)}> Update </button>}
           {props.updatingId === comment.id &&
             (<form>
               <input
@@ -20,14 +20,14 @@ export default function CommentsList(props) {
                 onChange={props.handleChange}
                 value={props.commentUpdateFormData.message}
                 placeholder="message" />
-              <input
+              {/* <input
                 type="text"
                 name="yaynay"
                 onChange={props.handleChange}
                 value={props.commentUpdateFormData.yaynay}
-                placeholder="yay(t) or nay(f)" />
-              <button onClick={props.handleSubmit}> Finalize </button>
-              <button onClick={props.handleCancel}> Cancel </button>
+                placeholder="yay(t) or nay(f)" /> */}
+              <button className='finalizeSubmit' onClick={props.handleSubmit}> Finalize </button>
+              <button className='cancelSubmit' onClick={props.handleCancel}> Cancel </button>
             </form>)}
         </div>)}
     </>
