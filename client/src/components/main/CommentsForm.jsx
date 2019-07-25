b3f553616c7511c67e8cb354aad0a586b640ea8f
jasmine carpentier <jascarpentier@gmail.com>
2019-07-25 11:35:16 -0400

    init

--- client/src/components/main/CommentsForm.jsx
+++ client/src/components/main/CommentsForm.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const CommentsForm = (props) => {
   return (
     <form>
-      <h1 className='commenth1'>Favourite thing to order?</h1>
+      <h1 className='commenth1'>Have something</h1>
       <input name="message" type="text" placeholder="text here ..." onChange={props.handleChange} />
       <input name="yaynay" type="text" placeholder=" " onChange={props.handleChange} />
       <input type="submit" placeholder="Submit" onClick={props.handleSubmit} />
