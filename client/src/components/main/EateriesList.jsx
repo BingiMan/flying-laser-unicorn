60ecc10d6ce03084834f2876376506ea13da186d
XX <xx@XXs-MBP13.local>
2019-07-25 15:20:25 -0400

    siri is awesome

--- client/src/components/main/EateriesList.jsx
+++ client/src/components/main/EateriesList.jsx
@@ -2,15 +2,23 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function EateriesList(props) {
+
   return (
     <>
+
+      <div className="backdrop-wrapper">
+        <div class="backdrop">
+          <p class="text lighten">ALL EATERIES</p>
+        </div>
+      </div>
+
       {props.eateries.map(eatery =>
-        <div className="eatery" key={eatery.id}>
-          <h3> <span>Name:</span> {eatery.name} </h3>
+        <div className="eatery eatery-list-item" key={eatery.id}>
+          <h3 className="knockout">{eatery.name.toUpperCase()} </h3>
           <p><span>Address:</span> {eatery.address}</p>
-          <p><span>Price Range:</span>{eatery.priceRange}</p>
-          <Link 
-          to= {`/single-eatery/${eatery.id}`}> Details </Link>
+          <p><span>Price Range:</span>{eatery.price_range}</p>
+          <Link id="detail-button"
+            to={`/single-eatery/${eatery.id}`}> Details </Link>
         </div>)}
     </>
   )
