23ec7430f663c89db4d9fb4d59d85af79453c8d5
XX <xx@XXs-MBP13.local>
2019-07-25 20:17:47 -0400

    changed the way price range is disaplyed on random eateries and eateries list

--- client/src/components/main/EateriesList.jsx
+++ client/src/components/main/EateriesList.jsx
@@ -3,6 +3,20 @@ import { Link } from 'react-router-dom';
 
 export default function EateriesList(props) {
 
+  const displayPrice = (str) => {
+    if (str === "1") {
+      return "Basically Free";
+    } else if (str === "2") {
+      return "Cheap as Chips";
+    } else if (str === "3") {
+      return "Won\'t Break the Bank";
+    } else if (str === "4") {
+      return "Fancy AF";
+    } else {
+      return "Not Sure";
+    }
+  };
+
   return (
     <>
       <div className="backdrop-wrapper">
@@ -16,7 +30,7 @@ export default function EateriesList(props) {
           <h3 className="knockout">{eatery.name.toUpperCase()} </h3>
           <p><span>Address:</span> {eatery.address}</p>
           <p><span>Category:</span>{eatery.category}</p>
-          <p><span>Price Range:</span>{eatery.price_range}</p>
+          <p><span>Price Range:</span>{displayPrice(eatery.price_range)}</p>
           <Link id="detail-button"
             to={`/single-eatery/${eatery.id}`}> Details </Link>
         </div>)}
--- client/src/components/main/RandomEatery.jsx
+++ client/src/components/main/RandomEatery.jsx
@@ -27,6 +27,21 @@ export default class RandomEatery extends React.Component {
 
   };
 
+  displayPrice = (str) => {
+    if (str === "1") {
+      return "Basically Free";
+    } else if (str === "2") {
+      return "Cheap as Chips";
+    } else if (str === "3") {
+      return "Won\'t Break the Bank";
+    } else if (str === "4") {
+      return "Fancy AF";
+    } else {
+      return "Not Sure";
+    }
+  }
+
+
   render() {
     return (
       <>
@@ -35,7 +50,7 @@ export default class RandomEatery extends React.Component {
           <div className="random-eatery-name">{this.state.eatery.name}</div>
           <p> <span>Address:</span> {this.state.eatery.address}</p>
           <p> <span>Category:</span> {this.state.eatery.category}</p>
-          <p> <span>Price Range:</span> {this.state.eatery.price_range}</p>
+          <p> <span>Price Range:</span> {this.displayPrice(this.state.eatery.price_range)}</p>
           <Link id="detail-button"
             to={`/single-eatery/${this.state.eatery.id}`}> Details </Link>
         </div>
